23ff9c600920289993c9ce0772dbf89f4d45d6c2
Timothy J Mahoney <38438323+Timmahoney2000@users.noreply.github.com>
2023-11-19 08:32:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ For example, if this array were passed as an argument:
 Your function would return the following array:
 
 ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
-The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated
 */
 
 //filter method
